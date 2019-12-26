621106229e25630b102be6289cfe1776a052a85d
中村 育豊 <akatsuki.unknow@gmail.com>
2019-12-26 16:07:15 +0900

    変数名の変更

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const scroll = new SmoothScroll('a[href*="#"]', {
     popstate: true,
     emitEvents: true
 });
+
 let BeforeEvent;
 document.addEventListener("scrollStart", event => {
     if (BeforeEvent !== undefined) {
@@ -16,28 +17,28 @@ document.addEventListener("scrollStart", event => {
 
 const termBase = document.getElementsByClassName("term")[0].children;
 const headerBase = document.getElementById("header");
-const chapterHeader = document.getElementById("chapterHeader");
-const articleHeader = document.getElementById("articleHeader");
+const chapterHeader = document.getElementById("chapter_header");
+const articleHeader = document.getElementById("article_header");
 let chapterNum = 0;
 function onScroll() {
-    let HeaderText, ArticleText;
+    let headerText, articleText;
 
     for(let i = 0; i < termBase.length; i++){
         let b = termBase[i].getBoundingClientRect().top - headerBase.offsetHeight - 100;
         if(b <= 0){
-            HeaderText = termBase[i].getElementsByTagName("h2")[0].innerHTML;
+            headerText = termBase[i].getElementsByTagName("h2")[0].innerHTML;
             chapterNum = i;
         }
     }
-    if(HeaderText == undefined) HeaderText = termBase[0].getElementsByTagName("h2")[0].innerHTML;
-    chapterHeader.innerHTML = HeaderText;
+    if(headerText == undefined) headerText = termBase[0].getElementsByTagName("h2")[0].innerHTML;
+    chapterHeader.innerHTML = headerText;
 
     for(let i = 0; i < termBase[chapterNum].getElementsByTagName("h4").length; i++){
         let b = termBase[chapterNum].getElementsByTagName("h4")[i].getBoundingClientRect().top - headerBase.offsetHeight - 100;
-        if(b <= 0)ArticleText = termBase[chapterNum].getElementsByTagName("h4")[i].innerHTML;
+        if(b <= 0)articleText = termBase[chapterNum].getElementsByTagName("h4")[i].innerHTML;
     }
-    if(ArticleText == undefined) ArticleText = termBase[chapterNum].getElementsByTagName("h4")[0].innerHTML;
-    articleHeader.innerHTML = ArticleText;
+    if(articleText == undefined) articleText = termBase[chapterNum].getElementsByTagName("h4")[0].innerHTML;
+    articleHeader.innerHTML = articleText;
 }
 onScroll();
 window.onscroll = onScroll;
